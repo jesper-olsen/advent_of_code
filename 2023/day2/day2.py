35ae5804121fcb6c8036e6e921504003ceba0709
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2023-12-02 10:31:36 -0600

    day2,prob1

---
+++ 2023/day2/day2.py
@@ -0,0 +1,28 @@
+import glob
+#Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+def sample2dict(sample):
+    terms = [term.split() for term in sample]
+    terms = {label:int(cnt) for (cnt,label) in terms}
+    return terms
+
+def read_games(fname):
+    g=(line.strip() for line in open(fname))
+    g=(line[line.index(':')+1:].split(';') for line in g if line!="")
+    g=([sample.split(',') for sample in samples] for samples in g)
+
+    for samples in g:
+        yield [sample2dict(sample) for sample in samples]
+ 
+def possible(bag, samples):
+    for key in bag:
+        for sample in samples:
+            if key in sample and bag[key]-sample[key]<0:
+                return False
+    return True
+     
+for fname in glob.glob("day2*input*.txt"):
+    d={"red":12, "green": 13, "blue":14}
+    g=((i+1,possible(d,game)) for (i,game) in enumerate(read_games(fname)))
+    g=(i for (i,flag) in g if flag)
+    print(f"Problem 1, {fname}: {sum(g)}")
