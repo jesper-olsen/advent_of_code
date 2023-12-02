1cb9a2c8a9a509807f23747db4fc9659419b79ab
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2023-12-02 12:44:42 -0600

    day2, problem 2

--- 2023/day2/day2.py
+++ 2023/day2/day2.py
@@ -1,4 +1,7 @@
 import glob
+from collections import defaultdict
+import functools
+import operator
 #Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
 def sample2dict(sample):
@@ -14,15 +17,29 @@ def read_games(fname):
     for samples in g:
         yield [sample2dict(sample) for sample in samples]
  
+def maxsamples(samples):
+    m=defaultdict(int)
+    for sample in samples:
+        for key in sample:
+            m[key]=max(m[key], sample[key])
+    return m
+
 def possible(bag, samples):
+    d={"red":12, "green": 13, "blue":14}
     for key in bag:
         for sample in samples:
             if key in sample and bag[key]-sample[key]<0:
                 return False
     return True
      
+foldl = lambda func, acc, xs: functools.reduce(func, xs, acc)
+
 for fname in glob.glob("day2*input*.txt"):
     d={"red":12, "green": 13, "blue":14}
     g=((i+1,possible(d,game)) for (i,game) in enumerate(read_games(fname)))
     g=(i for (i,flag) in g if flag)
     print(f"Problem 1, {fname}: {sum(g)}")
+
+    g=((i+1,maxsamples(game)) for (i,game) in enumerate(read_games(fname)))
+    N=sum(foldl(operator.mul, 1, d.values()) for (i,d) in g)
+    print(f"Problem 2, {fname}: {N}") 
