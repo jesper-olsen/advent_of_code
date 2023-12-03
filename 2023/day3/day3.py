b770f9da7107d68908c9542335c62bad456c10b5
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2023-12-03 14:57:41 -0600

    -HEIGHT

--- 2023/day3/day3.py
+++ 2023/day3/day3.py
@@ -13,10 +13,10 @@ def extract_number(x, line):
         x2+=1
     return x1,int(line[x1:x2+1])
      
-def find_numbers(x,y, lines, WIDTH, HEIGHT):
+def find_numbers(x,y, lines, WIDTH):
     used=set()
     for (z,k) in [(x+u,y+v) for u in (-1,0,1) for v in (-1,0,1) if (u,v)!=(0,0)]:
-        if z>=0 and z<WIDTH and k>=0 and k<HEIGHT:
+        if z>=0 and z<WIDTH and k>=0 and k<len(lines):
              if lines[k][z].isdigit():
                 q,n=extract_number(z, lines[k])
                 if not (q,k) in used:
@@ -26,16 +26,15 @@ def find_numbers(x,y, lines, WIDTH, HEIGHT):
 for fname in glob.glob("day3_input*.txt"):
     lines=[line.strip() for line in open(fname)]
     WIDTH=len(lines[0])
-    HEIGHT=len(lines)
     SYMBOLS=((x,y) for (y,line) in enumerate(lines) for (x,c) in enumerate(line) if c not in "0123456789.")
-    N=sum([n for (x,y) in SYMBOLS for n in find_numbers(x,y,lines,WIDTH,HEIGHT)])
+    N=sum([n for (x,y) in SYMBOLS for n in find_numbers(x,y,lines,WIDTH)])
     print(f"Problem 1, {fname}: {N}")
 
     GEARS=((x,y) for (y,line) in enumerate(lines) for (x,c) in enumerate(line) if c in "*")
 
     N=0
     for (x,y) in GEARS:
-        l=[n for n in find_numbers(x,y, lines, WIDTH, HEIGHT)]
+        l=[n for n in find_numbers(x,y, lines, WIDTH)]
         if len(l)>1:
             N+=functools.reduce(operator.mul, l, 1)
  
