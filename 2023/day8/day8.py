0de3d41e3293257618a4587a79d3a9ded38907bd
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2023-12-08 21:22:41 -0600

    lint

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -1,25 +1,25 @@
+""" advent of code 2023-12-8
+answers: input0 part1:     6 part2:              6
+answers: input1 part1: 18023 part2: 14449445933179
+"""
 import glob
 import itertools
 import math
 
-# answers: input0 part1:     6 part2:              6
-# answers: input1 part1: 18023 part2: 14449445933179
-
-def steps_to_end(node, network, RL, end_nodes):
-    for i, rl in enumerate(itertools.cycle(RL)):
+def steps_to_end(node, network, inst, end_nodes):
+    for i, rl in enumerate(itertools.cycle(inst)):
         node = network[node][rl]
         if node in end_nodes:
             return i + 1
 
 for fname in sorted(glob.glob("day8_input?.txt")):
-    g = (line.strip() for line in open(fname))
-    RL = [0 if c == "L" else 1 for c in next(g)]
-    g = ((line[:3], line[7:10], line[12:15]) for line in g if line != "")
-    network = {node: (l, r) for (node, l, r) in g}
+    g = (line.strip() for line in open(fname, encoding="ascii"))
+    inst = [0 if c == "L" else 1 for c in next(g)]
+    network = {line[:3]: (line[7:10], line[12:15]) for line in g if line != ""}
 
-    print(f"{fname} part1: ", steps_to_end("AAA", network, RL, ["ZZZ"]))
+    print(f"{fname} part1: ", steps_to_end("AAA", network, inst, ["ZZZ"]))
 
     start_nodes = [node for node in network.keys() if node[-1] == "A"]
     end_nodes = [node for node in network.keys() if node[-1] == "Z"]
-    l = [steps_to_end(node, network, RL, end_nodes) for node in start_nodes]
-    print(f"{fname} part2: ", math.lcm(*l)) 
+    l = [steps_to_end(node, network, inst, end_nodes) for node in start_nodes]
+    print(f"{fname} part2: ", math.lcm(*l))
